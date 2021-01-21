939cb0c54f29bb6cbd2b40fccfe742d021a858f6
Rahul Grover <rahulgrover99@gmail.com>
2021-01-21 16:47:25 +0530

    Getting started.

---
+++ helloworld/main.go
@@ -0,0 +1,11 @@
+package main
+
+// Package -> Can have many related files. This file belongs to package main
+// Why main? -> 2 types of package Executable and Reusable. Main is used to create executable.
+
+import "fmt"
+
+// package main also must have func main
+func main() {
+	fmt.Println("Hi")
+}
